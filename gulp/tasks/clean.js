bfbd5c82dbe24599541fa22c6040eebf8416ad04
brunty <openblue555@gmail.com>
2015-03-29 22:58:06 +0100

    Clean task

---
+++ gulp/tasks/clean.js
@@ -0,0 +1,8 @@
+var gulp = require('gulp');
+var del = require('del');
+var config = require('../config');
+
+// Our Default task
+gulp.task('clean', function(cb) {
+    del([config.paths.output.css], cb);
+});
